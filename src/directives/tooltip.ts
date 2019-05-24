0b878a36c029bc844ab86c6bb1c65740acb15683
neferent <neferentlive@gmail.com>
2019-05-24 11:15:47 -0700

    switching branches

--- src/directives/tooltip.ts
+++ src/directives/tooltip.ts
@@ -13,14 +13,11 @@ function queryElement(el) {
 
 function initHover(el,) {
   let rect = queryElement(el);
-  let parent = queryElement(el.parentElement);
   let hover = document.createElement("div");
   hover.style.position = "absolute";
-  hover.style.opacity = ".5";
   hover.style.width = rect.width + "px";
   hover.style.height = rect.height + "px";
   hover.style.top = "inherit";
-  hover.style.background = "red";
   hover.style.left = "inherit";
   hover.style.zIndex = "40";
   hover.className = "s-tooltip__hover";
@@ -30,73 +27,36 @@ function initHover(el,) {
 }
 
 function handleMouseOver(el) {
-
     el.querySelector(".s-tooltip").classList.add("s-tooltip-visible");
-
 }
 
 function handleMouseOut(el) {
-
-
   el.querySelector(".s-tooltip").classList.remove("s-tooltip-visible");
-
 }
-
-
-
 
 function getPlacement(el) {
   let element = el.getBoundingClientRect();
   let tooltip = el.parentElement.querySelector("div.s-tooltip").getBoundingClientRect();
   let placement = {
     topOffset: -Math.abs(tooltip.height + 8),
-    leftOffset: -Math.abs(element.width / 2 - tooltip.width / 2),
+    leftOffset: (element.width / 2 - tooltip.width / 2),
     elementWidth: element.width,
     elementHeight: element.height
   };
   return placement;
 }
 
-
-
-
-
-
-
-
-
-
-
-
 function initTooltip(el, binding) {
-
-
-
-
-
   let tooltip = document.createElement("div");
   tooltip.style.left = "inherit";
   tooltip.style.top = "inherit";
   tooltip.className = "s-tooltip";
-
   tooltip.innerHTML = "<div class=\"s-tooltip__label\">" + binding.value + "</div><div class=\"s-tooltip__caret\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 5\" width=\"16px\" height=\"5px\"><path d=\"M16 0c-1.11 0-2.17.44-2.96 1.23l-2.83 2.85C8.99 5.31 7 5.31 5.78 4.08L2.96 1.23C2.17.44 1.11 0 0 0h16z\"></path></svg></div>";
-
   el.insertAdjacentElement("afterbegin", tooltip);
-
   let placement = getPlacement(el);
-
   tooltip.style.transform = "translate(" + placement.leftOffset + "px," + placement.topOffset + "px)";
 
-  
 }
-
-
-
-
-
-
-
-
 
 const Tooltip: DirectiveOptions = {
     inserted(el, binding) {
